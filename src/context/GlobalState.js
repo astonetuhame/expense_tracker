fdb9a780418b4fedf2111646b97bfa4c9eb37544
Tuhame Astone <astonetuhame@gmail.com>
2021-12-14 18:00:15 +0300

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,7 +18,22 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({ children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (<GlobalContext.Provider value={{ transactions: state.transactions }}>
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    return (<GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction, addTransaction }}>
         {children}
     </GlobalContext.Provider>)
 }
